46e54c0ac03cb7a8036db82e40f3f7fda8b4092a
Allison Simon <ars287@miami.edu>
2018-11-20 17:57:58 -0500

    pushing a6 to github

---
+++ a6/assets/js/bali.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
+    indicators: true
+    });
+
+    $("button.bluebtn").click(function(){
+        document.location = 'https://www.cheapflights.com/flights-to-bali/?gclid=EAIaIQobChMIm9jjva3f3gIVvf_jBx3SKQAoEAAYAyAAEgJMcPD_BwE&gclsrc=aw.ds';
+        return false;
+    });
+  
