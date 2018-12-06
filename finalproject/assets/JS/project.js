b4c87d27f69a246df93d8f7c2a02cd9ebf97dad7
Allison Simon <ars287@miami.edu>
2018-12-06 16:37:21 -0500

    pushing new version of project

--- finalproject/assets/JS/project.js
+++ finalproject/assets/JS/project.js
@@ -1,28 +1,41 @@
 $(document).ready(function() {
 
-var myIndex = 0;
-carousel();
+  var myIndex = 0;
+  carousel();
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides")
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1};
-    x[myIndex-1].style.display = "block";
-    setTimeout(carousel, 3000);
+  function carousel() {
+      var i;
+      var x = document.getElementsByClassName("mySlides");
+      for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";
+      }
+      myIndex++;
+      if (myIndex > x.length) {myIndex = 1}
+      x[myIndex-1].style.display = "block";
+      setTimeout(carousel, 3000);
+  }
+
+  });
+  function random_item(items)
+{
+
+return items[Math.floor(Math.random()*items.length)];
+
 }
 
-$('.carousel.carousel-slider').carousel({
-  fullWidth: true
-  indicators: true
-  });
- });
+const items = ["Venom" , "Halloween" , "Killer Kate" , "Monster Party" , "Suspira" , "Instant Family" , "The Grinch" , "Night School" , "Green Book" , "The Long Dumb Road" , "A Star is Born" , "A Beautiful Boy" , "Bohemian Rhapsody" , "Boy Erased" , "Mary Queen of Scots" , "Robin Hood"];
+console.log("And the best movie award goes to: " + random_item(items));
 
- baguetteBox.run('.baguetteBoxFive', {
-         captions: function(element) {
-             return element.getElementsByTagName('img')[0].alt;
-         }
-     });
+//var randomValue = myArray[Math.floor(Math.random() * myArray.length)];
+
+//var movies = ["Venom", + "Halloween", + "Killer Kate", + "Monster Party", + "Suspira", "Instant Family", + "The Grinch", "Night School", "Green Book", "The Long Dumb Road", "A Star is Born", "A Beautiful Boy", "Bohemian Rhapsody", "Boy Erased", "Mary Queen of Scots", "Robin Hood" "Creed II", "The Girl in the Spider's Web", "Widows"];
+
+//var show = movies[Math.floor(Math.random() * movies.length)];
+
+
+
+
+
+  function goBack() {
+    window.history.back()
+  }
